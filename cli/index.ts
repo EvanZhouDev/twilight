d44a90f33c5638f73b8634800f003d8fb52c7003
Evan Zhou <evanzhoudev@gmail.com>
2024-12-22 10:31:49 -0800

    ✨ Added CLI logic

---
+++ cli/index.ts
@@ -0,0 +1,26 @@
+import run from "cli/runFile";
+import startRepl from "cli/repl";
+import chalk from "chalk";
+import stdlib from "lang/libs/stdlib";
+
+const args = process.argv.slice(2);
+
+if (!args[0]) {
+	startRepl();
+} else if (args[0] === "--help" || args[0] === "-h") {
+	console.log(`${chalk.magentaBright.bold(
+		"Twilight"
+	)} is a lambda calculus runtime and REPL.
+
+${chalk.magentaBright("Execute a file")}: twilight <file> [...flags]
+${chalk.blueBright("Start the REPL")}: twilight [...flags]
+
+Flags:
+${chalk.yellowBright("--help")}      Print this help message.
+`);
+} else if (args[0] === "--version" || args[0] === "-v") {
+	// TODO: make this directly tap from package.json
+	console.log("v0.1");
+} else {
+	run(args[0], [stdlib]);
+}
