b29bb348eba36e5865ba291eb3394eff52e139ea
Evan Zhou <evanzhoudev@gmail.com>
2024-12-22 10:30:48 -0800

    ✨ Added ability to import and run files

---
+++ lang/runtime/import.ts
@@ -0,0 +1,73 @@
+import stdlib, { type Library } from "lang/libs/stdlib";
+import { Environment } from "./env";
+import * as path from "node:path";
+import * as fs from "node:fs";
+import {
+	CyclicalImportError,
+	NonexistentImportedFileError,
+	NonexistentImportError,
+} from "lang/error";
+import { TwilightFormatter } from "lang/formatter";
+import { run } from "lang/runtime/run";
+import type { Module } from "lang/libs/stdlib";
+
+export default ({
+	location,
+	env = new Environment(),
+	libraries = [stdlib],
+	importHistory,
+}: {
+	location: string;
+	importHistory: string[];
+	env?: Environment;
+	libraries?: Library[];
+}): Module => {
+	if (location.split(".").at(-1) === "twi") {
+		// If the location is another Twilight file
+		const filePath = path.resolve(path.dirname(importHistory.at(-1)), location);
+
+		if (!fs.existsSync(filePath)) {
+			throw new NonexistentImportedFileError({
+				importName: filePath,
+				importHistory,
+			});
+		}
+
+		if (importHistory.includes(filePath)) {
+			throw new CyclicalImportError({ importPath: filePath, importHistory });
+		}
+
+		const tempEnv: Environment = new Environment();
+
+		run({
+			source: fs.readFileSync(filePath, "utf-8"),
+			env: tempEnv,
+			importHistory: [...importHistory, filePath],
+			formatter: new TwilightFormatter(),
+			libraries: [stdlib],
+		});
+		env.merge(tempEnv);
+		return {
+			static: tempEnv.static,
+			patterns: tempEnv.patterns,
+			dynamic: tempEnv.dynamic,
+		};
+	}
+
+	let included: Module;
+	for (const lib of libraries) {
+		if (lib[location]) {
+			env.merge(lib[location]);
+			included = lib[location];
+			return lib[location];
+		}
+	}
+
+	if (!included) {
+		throw new NonexistentImportError({
+			importName: location,
+			importHistory,
+			libraries,
+		});
+	}
+};
